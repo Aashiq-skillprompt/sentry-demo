d6fad1c112e415dd19bce590b6b14b34cfd0cb88
aashiqchalise <asq.chalise@gmail.com>
2023-08-25 16:52:11 +0545

    changing c to C in card

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 // import skillLogo from "./assets/logo.png";
 // import roundedLogo from "./assets/rounded-logo.png";
 import HomePage from "./Components/HomePage";
-import Card from "./Components/card";
+import Card from "./Components/Card";
 
 function App() {
   return (
